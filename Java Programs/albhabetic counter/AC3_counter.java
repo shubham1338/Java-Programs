4d069f4705cfbb6b32e9546b2babc71107cef289
shubham1338 <122975454+shubham1338@users.noreply.github.com>
2024-12-16 14:09:31 +0530

    Add files via upload

---
+++ Java Programs/albhabetic counter/AC3_counter.java
@@ -0,0 +1,24 @@
+package albhabetic counter;
+
+public class AC3_counter {
+    public static void main(String[] args) {
+        int n=5;
+        int count=n;
+        /*
+            A
+           FB
+          JGC
+         MKHD
+        ONLIE 
+         */
+        for(int i=)
+        {
+            for()
+            {
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    
+}
